8b251961e013c0f47d781422a4c9c6189ee3054c
onovich <hjn1110@163.com>
2024-04-10 06:17:49 +0800

    <feature> add: Speaker / Story

---
+++ Assets/com.tenon.rhyme/Scripts_Runtime/Speaker.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TenonKit.Rhyme {
+
+    public class Speaker {
+
+        // Data
+        Story story;
+
+        // State
+        bool isPlaying;
+
+    }
+
+}
---
+++ Assets/com.tenon.rhyme/Scripts_Runtime/Story.cs
@@ -0,0 +1,23 @@
+namespace TenonKit.Rhyme {
+
+    public class Story {
+
+        // Main data
+        string content;
+
+        // Avatar data
+        string name;
+        string avatarName;
+        float pitchName;
+
+        // Voice data
+        string voiceName;
+        float volume;
+
+        // Timing
+        float delay;
+        float speed;
+
+    }
+
+}
