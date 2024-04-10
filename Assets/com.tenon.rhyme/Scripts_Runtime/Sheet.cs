f21aab3d09e98f5d541d0190b0a185dcdb7aa398
onovich <hjn1110@163.com>
2024-04-10 16:51:28 +0800

    <refactor> nodify: Speaker -> Choir; Story -> Sheet

---
+++ Assets/com.tenon.rhyme/Scripts_Runtime/Sheet.cs
@@ -0,0 +1,23 @@
+namespace TenonKit.Rhyme {
+
+    public struct Sheet {
+
+        // Main data
+        public string content;
+
+        // Avatar data
+        public string name;
+        public string avatarName;
+        public float pitchName;
+
+        // Voice data
+        public string voiceName;
+        public float volume;
+
+        // Timing
+        public float delay;
+        public float speed;
+
+    }
+
+}
